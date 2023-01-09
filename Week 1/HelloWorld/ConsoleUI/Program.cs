982acd97c3dac2f3cc345d6aa268a367e2865810
Drew Tadlock <tadlockd7379@ad.faytechcc.edu>
2023-01-09 11:46:43 -0500

    Initial Week One Assignment

---
+++ Week 1/HelloWorld/ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Drew Tadlock.");
+            Console.WriteLine("I'm taking this class to become more proficient in C# and to get the credit.");
+            Console.WriteLine("I've only taken CTI-110 but I'm taking more python and swift along side this class.");
+            Console.WriteLine("All of the list was gone over in the other classes.");
+            Console.ReadLine();
+        }
+    }
+}
